d36c8484ba70866b384901678b3db15e18d5aff1
Sebastian Granda Gallego <sgg10.develop@gmail.com>
2024-03-11 07:38:07 -0500

    add setup description

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
+import os
 from setuptools import find_packages, setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 def read_requirements():
@@ -11,12 +16,22 @@ def read_requirements():
 
 setup(
     name="docscribe",
-    version="0.1.0",
+    version="0.1.1",
     description="A simple tool to generate documentation, reports, and more from your codebase.",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+    ],
     packages=find_packages(),
     include_dirs=".",
     include_package_data=True,
     install_requires=read_requirements(),
+    url="https://github.com/sgg10/docscribe/",
     license="MIT",
     author="sgg10",
     author_email="sgg10.develop@gmail.com",
@@ -25,4 +40,9 @@ setup(
         docscribe=app.main:cli
     """,
     python_requires=">=3.11",
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/sgg10/docscribe/issues",
+        "Source": "https://github.com/sgg10/docscribe/",
+        "Repository": "https://github.com/sgg10/docscribe/",
+    },
 )
